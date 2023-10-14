746f040eda48ee24bcf3ef30dceec8c57596cec9
SumiAkter2 <akteralisha36@gmail.com>
2023-10-14 20:34:51 +0600

    added Header.jsx

---
+++ src/Component/Shared/Header.jsx
@@ -0,0 +1,46 @@
+import { MdMenu } from "react-icons/md";
+import logo from "../../assets/image/logo/logo.png";
+const Header = () => {
+  const navItem = (
+    <>
+      <li className="mr-2">
+        <a href="/">Company</a>
+      </li>
+      <li className="mr-2">
+        <a href="/">Projects</a>
+      </li>
+      <li className="mr-2">
+        <a href="/contact">Contact</a>
+      </li>
+    </>
+  );
+  return (
+    <div className="max-w-screen navbar  font-bold p-4">
+      <div className="navbar-start">
+        <a href="/ " className=" flex justify-center items-center">
+          <img src={logo} alt="logo" className="" />
+        </a>
+      </div>
+      {/* <div className="navbar-end"></div> */}
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal  hidden lg:flex text-red-500 ">
+          {navItem}
+        </ul>
+        {/* mobile nav */}
+        <div className="dropdown dropdown-end ">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <MdMenu color="#1ba9ca" size="25px" />
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm  dropdown-content mt-3 z-[1] p-2   rounded-box w-52 text-white bg-black gap-y-4"
+          >
+            {navItem}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,11 +1,15 @@
-
+import { Outlet } from "react-router-dom";
+import Header from "../../Component/Shared/Header";
+import Footer from "../../Component/Shared/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
