eefb19c39e932249ee4ef9c29a7f0c2f537b5c71
SumiAkter2 <akteralisha36@gmail.com>
2023-10-15 09:23:00 +0600

    page transition done

--- src/Component/PageTransition.jsx
+++ src/Component/PageTransition.jsx
@@ -1,12 +1,25 @@
 import { motion } from "framer-motion";
 const PageTransition = ({ children }) => {
+  const opacityAnimation = {
+    initial: { opacity: 0, scale: 0 },
+    animate: { opacity: 1, scale: 1 },
+    exit: { opacity: 0, scale: 0 },
+  };
+  // const rotateAnimation = {
+  //   initial: { rotateY: 0 },
+  //   animate: { rotateY: 90 },
+  //   exit: {
+  //     rotateY: -90,
+  //   },
+  // };
   return (
     <motion.div
       className="slide-out"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+      transition={{ ease: "easeInOut", duration: 0.4 }}
+      variants={opacityAnimation}
+      initial="initial"
+      animate="animate"
+      exit="exit"
     >
       {children}
     </motion.div>
--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -1,19 +1,18 @@
 import PageTransition from "../PageTransition";
-
+import img from "../../assets/image/Home/photo-1508919801845-fc2ae1bc2a28.jpg";
 const Project = () => {
   return (
     <PageTransition>
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          backgroundImage: `url("${img}")`,
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">From project there</h1>
             <p className="mb-5">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
